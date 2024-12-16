41052e0eeb0aec5f5bc68711208dd92950b0474a
Gheorghe <madangheorghe.30@gmail.com>
2024-12-16 13:20:35 +0100

    second ex

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,17 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+let name = "Gheorghe"
+console.log(name)
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+let number = 2
+let text = "testo"
+let bool = true;
+console.log(number, text, bool)
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
